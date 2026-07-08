dd30a4c6dfbd0da1cef50bab8a18c10a4d912887
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-08 09:56:16 +0200

    request body + string validations

--- 01-tutorial-basico-fastapi/09-request-body.py
+++ 01-tutorial-basico-fastapi/09-request-body.py
@@ -4,9 +4,12 @@ https://fastapi.tiangolo.com/tutorial/body/
 
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/user-guide/request-body", tags=["Tutorial Básico de FastAPI"])
+router = APIRouter(
+    prefix="/user-guide/request-body", tags=["Tutorial Básico de FastAPI"]
+)
 
 # Agrega aquí el código de la lección de FastAPI
+
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +19,21 @@ async def read_lesson():
         "path": "/user-guide/request-body",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/body/",
     }
+
+
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@router.post("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "item": item, "q": q}
+
+    return {"item_id": item_id, "item": item}
--- 01-tutorial-basico-fastapi/10-query-parameters-and-string-validations.py
+++ 01-tutorial-basico-fastapi/10-query-parameters-and-string-validations.py
@@ -2,11 +2,17 @@
 https://fastapi.tiangolo.com/tutorial/query-params-str-validations/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/user-guide/query-parameters-and-string-validations", tags=["Tutorial Básico de FastAPI"])
+from fastapi import APIRouter, Query
+
+router = APIRouter(
+    prefix="/user-guide/query-parameters-and-string-validations",
+    tags=["Tutorial Básico de FastAPI"],
+)
 
 # Agrega aquí el código de la lección de FastAPI
+
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +22,25 @@ async def read_lesson():
         "path": "/user-guide/query-parameters-and-string-validations",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/query-params-str-validations/",
     }
+
+
+# Newer versions: Python 3.10+, FastAPI 0.95+ PREFERRED
+@router.get("/items")
+async def get_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    items = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+
+    if q:
+        items.update({"q": q})
+
+    return items
+
+
+# Older versions: Python 3.10- Non-Annotated, FastAPI 0.95- NON-PREFERRED
+@router.get("/items")
+async def get_items(q: str | None = Query(default=None, max_length=50)):
+    items = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+
+    if q:
+        items.update({"q": q})
+
+    return items
