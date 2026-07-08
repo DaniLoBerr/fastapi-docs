ee2dbe167b7cc89e9f1706800c86d02142b68475
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-08 12:59:35 +0200

    path parameters and st validation

--- 01-tutorial-basico-fastapi/10-query-parameters-and-string-validations.py
+++ 01-tutorial-basico-fastapi/10-query-parameters-and-string-validations.py
@@ -2,16 +2,16 @@
 https://fastapi.tiangolo.com/tutorial/query-params-str-validations/
 """
 
+from random import choice
 from typing import Annotated
 
 from fastapi import APIRouter, Query
+from pydantic import AfterValidator
 
 router = APIRouter(
     prefix="/user-guide/query-parameters-and-string-validations",
     tags=["Tutorial Básico de FastAPI"],
 )
-
-# Agrega aquí el código de la lección de FastAPI
 
 
 @router.get("/")
@@ -24,23 +24,112 @@ async def read_lesson():
     }
 
 
-# Newer versions: Python 3.10+, FastAPI 0.95+ PREFERRED
-@router.get("/items")
-async def get_items(q: Annotated[str | None, Query(max_length=50)] = None):
+# CODE EXAMPLES:
+
+
+# QUERY PARAMETER
+@router.get("/items-query-parameter")
+async def items_query_parameter(q: str | None = None):
+    items = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        items.update({"q": q})
+    return items
+
+
+# ADITIONAL VALIDATION
+#   Generic validations: alias, deprecated, description, title, exclude (include_in_schema)
+#   Validation specific for strings: max_length, min_length, pattern
+
+
+#   Newer versions: Python 3.10+, FastAPI 0.95+ PREFERRED
+@router.get("/aditional-validation-new")
+async def aditional_validation_new(
+    q: Annotated[
+        str | None,
+        Query(
+            alias="aditional-validation",
+            deprecated=True,
+            description="Example of Aditional Validation",
+            max_length=50,
+            min_length=3,
+            pattern="^fixedquery$",
+            title="Aditional Validation",
+        ),
+    ] = None,
+    hidden: Annotated[str | None, Query(include_in_schema=False)] = None,
+):
     items = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
 
     if q:
         items.update({"q": q})
 
+    if hidden:
+        items.update({"hidden_query": hidden})
+
     return items
 
 
-# Older versions: Python 3.10- Non-Annotated, FastAPI 0.95- NON-PREFERRED
-@router.get("/items")
-async def get_items(q: str | None = Query(default=None, max_length=50)):
+#   Older versions: Python 3.10- Non-Annotated, FastAPI 0.95- NON-PREFERRED
+@router.get("/aditional-validation-old")
+async def aditional_validation_old(
+    q: str | None = Query(
+        alias="aditional-validation",
+        default=None,
+        deprecated=True,
+        description="Example of Aditional Validation",
+        max_length=50,
+        min_length=3,
+        pattern="^fixedquery$",
+        title="Aditional Validation",
+    ),
+    hidden: str | None = Query(default=None, include_in_schema=False),
+):
     items = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
 
     if q:
         items.update({"q": q})
 
+    if hidden:
+        items.update({"hidden_query": hidden})
+
     return items
+
+
+# Custom Validation: AfterValidator, BeforeValidator, ...
+
+
+# Optional? None or any other default value
+# Required? Not adding default value. Can be None
+# You can recieve a list of values like:
+#   http://localhost:8000/items/?q=foo&q=bar
+#   But you have to declare the parameter as Query:
+@router.get("/aditional-validation-list")
+async def aditional_validation_list(q: Annotated[list[str], Query()] = ["foo", "bar"]):
+    query_items = {"q": q}
+    return query_items
+
+
+# CUSTOM VALIDATION
+#   AfterValidator
+data: dict[str, str] = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+
+@router.get("/custom-validation")
+async def custom_after_validation(
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = choice(list(data.items()))
+    return {"id": id, "name": item}
