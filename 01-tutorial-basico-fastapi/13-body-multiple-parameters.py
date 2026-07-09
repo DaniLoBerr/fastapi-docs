d96184cdde49d648aabe24f7c78e3004b4efe143
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-09 11:57:46 +0200

    13 body multiple parameters

--- 01-tutorial-basico-fastapi/13-body-multiple-parameters.py
+++ 01-tutorial-basico-fastapi/13-body-multiple-parameters.py
@@ -2,11 +2,15 @@
 https://fastapi.tiangolo.com/tutorial/body-multiple-params/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/user-guide/body-multiple-parameters", tags=["Tutorial Básico de FastAPI"])
+from fastapi import APIRouter, Body, Path
+from pydantic import BaseModel
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/user-guide/body-multiple-parameters", tags=["Tutorial Básico de FastAPI"]
+)
+
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +20,47 @@ async def read_lesson():
         "path": "/user-guide/body-multiple-parameters",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/body-multiple-params/",
     }
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@router.put("/mix-path-query-multiple-extra-body/{item_id}")
+async def mix_path_query_multiple_extra_body_request(
+    *,
+    item_id: Annotated[int, Path(title="The ID of the Item to get", gt=0, le=1000)],
+    item: Item | None = None,
+    user: User | None = None,
+    extra_body: Annotated[str, Body()],
+    q: str | None = None,
+):
+    results = {"item_id": item_id}
+    if item:
+        results.update({"item": item})
+    if q:
+        results.update({"q": q})
+    if user:
+        results.update({"user": user})
+    if extra_body:
+        results.update({"extra_body": extra_body})
+    return results
+
+
+# Embed
+@router.put("/embed/{item_id}")
+async def embed(
+    *,
+    item_id: Annotated[int, Path(title="The ID of the Item to get", gt=0, le=1000)],
+    specific_item: Annotated[Item, Body(embed=True)],
+):
+    results = {"item_id": item_id, "specific_item": specific_item}
+    return results
