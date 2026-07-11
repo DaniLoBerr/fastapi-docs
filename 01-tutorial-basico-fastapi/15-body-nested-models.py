e103750c80b4aedc562912cad43312f028d81c3a
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-11 10:05:00 +0200

    15 - Body nested models

--- 01-tutorial-basico-fastapi/15-body-nested-models.py
+++ 01-tutorial-basico-fastapi/15-body-nested-models.py
@@ -2,11 +2,15 @@
 https://fastapi.tiangolo.com/tutorial/body-nested-models/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/user-guide/body-nested-models", tags=["Tutorial Básico de FastAPI"])
+from fastapi import APIRouter, Body
+from pydantic import BaseModel, HttpUrl
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/user-guide/body-nested-models", tags=["Tutorial Básico de FastAPI"]
+)
+
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +20,45 @@ async def read_lesson():
         "path": "/user-guide/body-nested-models",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/body-nested-models/",
     }
+
+
+# Set types, Nested and deeply nested sub-models, special types
+
+
+class Image(BaseModel):
+    name: str
+    url: HttpUrl  # Special type
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+    img: list[Image] | None = None
+
+
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+
+@router.post("/nested-models")
+async def nested_models(offer: Annotated[Offer, Body(embed=True)]):
+    return offer
+
+
+# Bodies of pure lists
+@router.post("/pure-lists")
+async def pure_lists(images: list[Image]):
+    return images
+
+
+# Bodies of arbitrary dicts
+#   Pydantic models = campos fijos, dict = campos variables
+@router.post("/arbitrary-dicts")
+async def arbitrar_dicts(some_dicts: dict[int, str]):
+    return some_dicts
