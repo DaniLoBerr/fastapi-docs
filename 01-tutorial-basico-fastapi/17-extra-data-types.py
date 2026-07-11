3617117f53d115061f1e92d72690dfbcb15e6678
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-11 13:17:19 +0200

    17 - extra data types

--- 01-tutorial-basico-fastapi/17-extra-data-types.py
+++ 01-tutorial-basico-fastapi/17-extra-data-types.py
@@ -2,11 +2,12 @@
 https://fastapi.tiangolo.com/tutorial/extra-data-types/
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
-router = APIRouter(prefix="/user-guide/extra-data-types", tags=["Tutorial Básico de FastAPI"])
+router = APIRouter(
+    prefix="/user-guide/extra-data-types", tags=["Tutorial Básico de FastAPI"]
+)
 
-# Agrega aquí el código de la lección de FastAPI
 
 @router.get("/")
 async def read_lesson():
@@ -15,4 +16,40 @@ async def read_lesson():
         "lesson": "Extra Data Types",
         "path": "/user-guide/extra-data-types",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/extra-data-types/",
+    }
+
+
+# EXTRA DATA TYPES
+#   UUID
+#   datetime.datetime
+#   datetime.date
+#   datetime.time
+#   datetime.timedelta
+#   frozenset
+#   bytes
+#   Decimal
+
+from datetime import datetime, time, timedelta
+from typing import Annotated
+from uuid import UUID
+
+
+@router.put("/extra-data-types/{item_id}")
+async def extra_data_types(
+    item_id: UUID,
+    start_datetime: Annotated[datetime, Body()],
+    end_datetime: Annotated[datetime, Body()],
+    process_after: Annotated[timedelta, Body()],
+    repeat_at: Annotated[time, Body()],
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "process_after": process_after,
+        "repeat_at": repeat_at,
+        "start_process": start_process,
+        "duration": duration,
     }
