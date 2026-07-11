3b5254171a983ddc2b40b216384e76db1377b9c3
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-11 16:53:01 +0200

    19 - header parameters

--- 02-parametros-especiales/19-header-parameters.py
+++ 02-parametros-especiales/19-header-parameters.py
@@ -2,11 +2,15 @@
 https://fastapi.tiangolo.com/tutorial/header-params/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/special-params/header-parameters", tags=["Parámetros especiales, modelos en capas y respuestas"])
+from fastapi import APIRouter, Header
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/special-params/header-parameters",
+    tags=["Parámetros especiales, modelos en capas y respuestas"],
+)
+
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +20,24 @@ async def read_lesson():
         "path": "/special-params/header-parameters",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/header-params/",
     }
+
+
+# HEADER PARAMETERS
+@router.get("/headers")
+async def headers(user_agent: Annotated[str | None, Header()] = None):
+    return {"User-Agent": user_agent}
+
+
+# Possibility of disable underscore conversion: Header(convert_underscores=False)
+@router.get("/items/")
+async def read_items(
+    strange_header: Annotated[str | None, Header(convert_underscores=False)] = None,
+):
+    return {"strange_header": strange_header}
+
+
+# Duplicate Headers
+#   Receiving duplicate headers (several values of the same header) = list of strings
+@router.get("/duplicate-headers")
+async def duplicate_headers(x_token: Annotated[list[str], Header()]):
+    return {"X-Token values": x_token}
