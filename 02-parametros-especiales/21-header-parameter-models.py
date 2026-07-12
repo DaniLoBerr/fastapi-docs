35a4fb0625b6a6774e589a21b49477da93d3ec8f
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-12 12:50:50 +0200

    21 - Header parameter models

--- 02-parametros-especiales/21-header-parameter-models.py
+++ 02-parametros-especiales/21-header-parameter-models.py
@@ -2,11 +2,16 @@
 https://fastapi.tiangolo.com/tutorial/header-param-models/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/special-params/header-parameter-models", tags=["Parámetros especiales, modelos en capas y respuestas"])
+from fastapi import APIRouter, Header
+from pydantic import BaseModel
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/special-params/header-parameter-models",
+    tags=["Parámetros especiales, modelos en capas y respuestas"],
+)
+
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +21,28 @@ async def read_lesson():
         "path": "/special-params/header-parameter-models",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/header-param-models/",
     }
+
+
+# Pydantic Header Models
+
+#   You can create a Pydantic model to declare a related group of headers you want to
+#   receive in your path operation function.
+
+#   You can also forbid the client sending extra headers
+
+#   You can also disable the conversion of underscore to hyphens (Header(convert_underscores=False))
+
+
+class CommonHeaders(BaseModel):
+    # model_config = {"extra": "forbid"}
+
+    host: str
+    save_data: bool
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = []
+
+
+@router.get("/header-param-models")
+async def get_headers(headers: Annotated[CommonHeaders, Header()]):
+    return headers
