09b1d5b1b3518a56ec97eb18b6d016dc8fd9d794
danilober <daniellopez@elitesports17.com>
2026-07-17 12:42:57 +0200

    refactor lesson 1

--- 00-fundamentos-python-async-entorno/01-python-types-intro.py
+++ 00-fundamentos-python-async-entorno/01-python-types-intro.py
@@ -4,30 +4,39 @@ https://fastapi.tiangolo.com/python-types/
 
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/foundations/python-types-intro", tags=["Fundamentos de Python, async y entorno"])
+router = APIRouter(
+    prefix="/foundations/python-types-intro",
+    tags=["Fundamentos de Python, async y entorno"],
+)
 
 from datetime import datetime
-from pydantic import BaseModel
 from typing import Annotated
 
+from pydantic import BaseModel
 
+
+# Classes as types: Pydantic models
 class User(BaseModel):
     id: int
     name: str
     created_at: datetime | None = None
-    friends: list[Annotated[int, "A list of user_id"]] = []
+    friends: list[
+        Annotated[int, "A list of user_id"]
+    ] = []  # type hints with metadata annotations
+
 
 external_data = {
     "id": 1,
     "name": "John Doe",
     "created_at": datetime.now(),
-    "friends": [3, 7, 65]
+    "friends": [3, 7, 65],
 }
 
 user1 = User(**external_data)
 
 if __name__ == "__main__":
     print(user1.name)
+
 
 @router.get("/")
 async def read_lesson():
