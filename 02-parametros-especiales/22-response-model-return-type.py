c30b3e2c06aad3521ff3194c82fc6b61d978af1b
danilober <daniellopez@elitesports17.com>
2026-07-17 15:50:54 +0200

    lesson 22 schema

--- 02-parametros-especiales/22-response-model-return-type.py
+++ 02-parametros-especiales/22-response-model-return-type.py
@@ -4,9 +4,11 @@ https://fastapi.tiangolo.com/tutorial/response-model/
 
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/special-params/response-model-return-type", tags=["Parámetros especiales, modelos en capas y respuestas"])
+router = APIRouter(
+    prefix="/special-params/response-model-return-type",
+    tags=["Parámetros especiales, modelos en capas y respuestas"],
+)
 
-# Agrega aquí el código de la lección de FastAPI
 
 @router.get("/")
 async def read_lesson():
@@ -16,3 +18,28 @@ async def read_lesson():
         "path": "/special-params/response-model-return-type",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/response-model/",
     }
+
+
+# Response Model - Return Type
+# response_model parameter
+#   Return some data that it's not exactly what the type declares
+#   Example: return a dict but declare it as a Pydantic model
+
+# response_model priority
+# return the same input data
+# add an output model
+# response model or return type
+# return type and data filtering
+# type annotations and tooling
+# fastapi data filtering
+# other return type annotations
+# return a response directly
+# annotate a response subclass
+# Invalid Return Type Annotations
+# disable response model
+# Response Model encoding parameters
+# Use the response_model_exclude_unset parameter
+# Data with values for fields with defaults
+# Data with the same values as the defaults
+# response_model_include and response_model_exclude
+# Using lists instead of sets
