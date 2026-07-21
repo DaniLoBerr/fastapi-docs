e9f9e47a794f94dc97ed8150d167468bf8ce7d9d
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-21 19:32:43 +0200

    26 - Form Models

--- 03-formularios-ficheros-errores/26-form-models.py
+++ 03-formularios-ficheros-errores/26-form-models.py
@@ -2,17 +2,43 @@
 https://fastapi.tiangolo.com/tutorial/request-form-models/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/forms-files-errors/form-models", tags=["Formularios, ficheros y manejo de errores"])
+from fastapi import APIRouter, Form
+from pydantic import BaseModel
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/forms-files-errors/form-models",
+    tags=["Formularios, ficheros y manejo de errores"],
+)
+
 
 @router.get("/")
-async def read_lesson():
+async def read_lesson():  # noqa: ANN201
     return {
         "section": "Formularios, ficheros y manejo de errores",
         "lesson": "Form Models",
         "path": "/forms-files-errors/form-models",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/request-form-models/",
     }
+
+
+"""
+    NOTES: Form Models
+
+    Puedes crear un modelo Pydantic con los datos
+    que esperas recibir de un formulario.
+    También puedes prohibir campos extras.
+"""
+
+
+class FormData(BaseModel):
+    username: str
+    password: str
+
+    model_config = {"extra": "forbid"}
+
+
+@router.post("/post-form-model")
+async def post_form_model(data: Annotated[FormData, Form()]) -> FormData:
+    return data
