9c8e8af6861b5607dcc5eba065356610ae892405
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-07-23 07:58:39 +0200

    28 - Request forms and files

--- 03-formularios-ficheros-errores/28-request-forms-and-files.py
+++ 03-formularios-ficheros-errores/28-request-forms-and-files.py
@@ -2,17 +2,38 @@
 https://fastapi.tiangolo.com/tutorial/request-forms-and-files/
 """
 
-from fastapi import APIRouter
+from typing import Annotated, Any
 
-router = APIRouter(prefix="/forms-files-errors/request-forms-and-files", tags=["Formularios, ficheros y manejo de errores"])
+from fastapi import APIRouter, File, Form, UploadFile
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/forms-files-errors/request-forms-and-files",
+    tags=["Formularios, ficheros y manejo de errores"],
+)
+
 
 @router.get("/")
-async def read_lesson():
+async def read_lesson():  # noqa
     return {
         "section": "Formularios, ficheros y manejo de errores",
         "lesson": "Request Forms and Files",
         "path": "/forms-files-errors/request-forms-and-files",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/request-forms-and-files/",
     }
+
+
+"""
+    NOTES: Requests with Forms and Files
+    - When you want to receive data from a form, you can use Form.
+    - When you want to receive files, you can use File.
+    - You can use Form and File together in the same endpoint.
+"""
+
+
+@router.post("/post-files")
+async def post_files(
+    token: Annotated[str, Form()],
+    file_a: Annotated[bytes, File()],
+    file_b: Annotated[UploadFile, File()],
+) -> dict[str, Any]:
+    return {"token": token, "file_a": len(file_a), "file_b": file_b.filename}
