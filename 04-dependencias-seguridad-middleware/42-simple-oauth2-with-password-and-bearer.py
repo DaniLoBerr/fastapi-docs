393be16021f8bccb6eafc60cf6aebee49e1d3e2e
daniloberr <97476157+DaniLoBerr@users.noreply.github.com>
2026-09-04 08:13:00 +0200

    42 - simple oauth2 pt.1

--- 04-dependencias-seguridad-middleware/42-simple-oauth2-with-password-and-bearer.py
+++ 04-dependencias-seguridad-middleware/42-simple-oauth2-with-password-and-bearer.py
@@ -2,17 +2,92 @@
 https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
 """
 
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/dependencies-security-middleware/simple-oauth2-with-password-and-bearer", tags=["Dependencias, seguridad básica y middleware"])
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
-# Agrega aquí el código de la lección de FastAPI
+router = APIRouter(
+    prefix="/dependencies-security-middleware/simple-oauth2-with-password-and-bearer",
+    tags=["Dependencias, seguridad básica y middleware"],
+)
+
 
 @router.get("/")
-async def read_lesson():
+async def read_lesson():  # noqa
     return {
         "section": "Dependencias, seguridad básica y middleware",
         "lesson": "Simple OAuth2 with Password and Bearer",
         "path": "/dependencies-security-middleware/simple-oauth2-with-password-and-bearer",
         "reference_url": "https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/",
     }
+
+
+"""
+    ...
+
+"""
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedpassword",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedpassword",
+        "disabled": False,
+    },
+    "bob": {
+        "username": "bob",
+        "full_name": "Bob Marley",
+        "email": "bob@example.com",
+        "hashed_password": "fakehashedpassword",
+        "disabled": False,
+    },
+}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def fake_hash_password(password: str) -> str:
+    return "fakehashed" + password
+
+
+@router.get("/token")
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> dict[str, str]:
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+
+    return {"access_token": user.username, "type": "Bearer"}
